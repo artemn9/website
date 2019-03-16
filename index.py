0c6fba9d370e78ec655c540624374bc6709938a0
Artem Neklyudov <artem.neklyudov@gmail.com>
2019-03-16 22:07:37 +0100

    intermediate messages added.

--- index.py
+++ index.py
@@ -8,10 +8,14 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
 
+        self.wfile.write(str("Hello, getting ready. // ").encode())
         import pymongo
+        self.wfile.write(str("Pymongo inserted. // ").encode())
         client = pymongo.MongoClient("mongodb+srv://artemn9:rDkAH8Npm5XXHaPb@artemn9-mongo-fpjxf.mongodb.net/test?retryWrites=true")
         db = client.get_database('academic')
         cl = db.get_collection('papers')
+        self.wfile.write(str("Starting cycle. // ").encode())
         for doc in cl.find():
+            self.wfile.write(str("one ... ").encode())
             self.wfile.write(str(doc).encode())
         return
